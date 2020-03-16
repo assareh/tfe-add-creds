a32b7e4505f029266bf6bf05b2b5e7719eb4b61f
assareh <assareh@hashicorp.com>
2020-03-16 12:00:20 -0700

    finished implementing for_each

--- main.tf
+++ main.tf
@@ -13,57 +13,73 @@ resource "tfe_variable" "gcp_credentials" {
 }
 
 resource "tfe_variable" "aws_secret_access_key" {
-  count        = length(var.aws_workspace_ids)
+  for_each     = var.aws_workspace_ids
   key          = "AWS_SECRET_ACCESS_KEY"
   value        = var.aws_secret_access_key
   category     = "env"
   sensitive    = true
-  workspace_id = "${var.organization}/${element(var.aws_workspace_ids, count.index)}"
+  workspace_id = each.value
 }
 
 resource "tfe_variable" "aws_access_key_id" {
-  count        = length(var.aws_workspace_ids)
+  for_each     = var.aws_workspace_ids
   key          = "AWS_ACCESS_KEY_ID"
   value        = var.aws_access_key_id
   category     = "env"
   sensitive    = true
-  workspace_id = "${var.organization}/${element(var.aws_workspace_ids, count.index)}"
+  workspace_id = each.value
 }
 
 resource "tfe_variable" "arm_subscription_id" {
-  count        = length(var.azure_workspace_ids)
+  for_each     = var.azure_workspace_ids
   key          = "ARM_SUBSCRIPTION_ID"
   value        = var.arm_subscription_id
   category     = "env"
   sensitive    = true
-  workspace_id = "${var.organization}/${element(var.azure_workspace_ids, count.index)}"
+  workspace_id = each.value
 }
 
 resource "tfe_variable" "arm_client_secret" {
-  count        = length(var.azure_workspace_ids)
+  for_each     = var.azure_workspace_ids
   key          = "ARM_CLIENT_SECRET"
   value        = var.arm_client_secret
   category     = "env"
   sensitive    = true
-  workspace_id = "${var.organization}/${element(var.azure_workspace_ids, count.index)}"
+  workspace_id = each.value
 }
 
 resource "tfe_variable" "arm_tenant_id" {
-  count        = length(var.azure_workspace_ids)
+  for_each     = var.azure_workspace_ids
   key          = "ARM_TENANT_ID"
   value        = var.arm_tenant_id
   category     = "env"
   sensitive    = true
-  workspace_id = "${var.organization}/${element(var.azure_workspace_ids, count.index)}"
+  workspace_id = each.value
 }
 
 resource "tfe_variable" "arm_client_id" {
-  count        = length(var.azure_workspace_ids)
+  for_each     = var.azure_workspace_ids
   key          = "ARM_CLIENT_ID"
   value        = var.arm_client_id
   category     = "env"
   sensitive    = true
-  workspace_id = "${var.organization}/${element(var.azure_workspace_ids, count.index)}"
+  workspace_id = each.value
+}
+
+resource "tfe_variable" "env_vars_aws" {
+  for_each     = var.aws_workspace_ids
+  key          = "CONFIRM_DESTROY"
+  value        = "1"
+  category     = "env"
+  workspace_id = each.value
+}
+
+resource "tfe_variable" "env_vars_azure" {
+  for_each     = var.azure_workspace_ids
+  key          = "CONFIRM_DESTROY"
+  value        = "1"
+  category     = "env"
+  workspace_id = each.value
 }
 
 resource "tfe_variable" "env_vars_gcp" {
