1d8fa296e73e9b3b1eecb94ce105144b0821a533
assareh <assareh@hashicorp.com>
2020-03-16 12:14:10 -0700

    corrected workspace format to include org name

--- assareh.auto.tfvars
+++ assareh.auto.tfvars
@@ -1,3 +1,5 @@
-aws_workspace_ids = ["ws-wd7ot4BKCkn6yohn"]
+aws_workspace_ids = ["ws-wd7ot4BKCkn6yohn"] // aws-ec2-instance-dev
+
 azure_workspace_ids = []
-gcp_workspace_ids = ["ws-vKw3XxThAu9arf6h"]
+
+gcp_workspace_ids = ["ws-vKw3XxThAu9arf6h"] // gcp-compute-instance-dev
--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "tfe_variable" "gcp_credentials" {
   value        = var.gcp_credentials
   category     = "env"
   sensitive    = true
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "aws_secret_access_key" {
@@ -18,7 +18,7 @@ resource "tfe_variable" "aws_secret_access_key" {
   value        = var.aws_secret_access_key
   category     = "env"
   sensitive    = true
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "aws_access_key_id" {
@@ -27,7 +27,7 @@ resource "tfe_variable" "aws_access_key_id" {
   value        = var.aws_access_key_id
   category     = "env"
   sensitive    = true
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "arm_subscription_id" {
@@ -36,7 +36,7 @@ resource "tfe_variable" "arm_subscription_id" {
   value        = var.arm_subscription_id
   category     = "env"
   sensitive    = true
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "arm_client_secret" {
@@ -45,7 +45,7 @@ resource "tfe_variable" "arm_client_secret" {
   value        = var.arm_client_secret
   category     = "env"
   sensitive    = true
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "arm_tenant_id" {
@@ -54,7 +54,7 @@ resource "tfe_variable" "arm_tenant_id" {
   value        = var.arm_tenant_id
   category     = "env"
   sensitive    = true
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "arm_client_id" {
@@ -63,7 +63,7 @@ resource "tfe_variable" "arm_client_id" {
   value        = var.arm_client_id
   category     = "env"
   sensitive    = true
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "env_vars_aws" {
@@ -71,7 +71,7 @@ resource "tfe_variable" "env_vars_aws" {
   key          = "CONFIRM_DESTROY"
   value        = "1"
   category     = "env"
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "env_vars_azure" {
@@ -79,7 +79,7 @@ resource "tfe_variable" "env_vars_azure" {
   key          = "CONFIRM_DESTROY"
   value        = "1"
   category     = "env"
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
 resource "tfe_variable" "env_vars_gcp" {
@@ -87,6 +87,6 @@ resource "tfe_variable" "env_vars_gcp" {
   key          = "CONFIRM_DESTROY"
   value        = "1"
   category     = "env"
-  workspace_id = each.value
+  workspace_id = "${var.organization} + ${each.value}"
 }
 
